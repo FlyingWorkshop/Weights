f204945b529dff68dd9d0d7e0e86bcd52b646e9b
FlyingWorkshop <76822456+FlyingWorkshop@users.noreply.github.com>
2021-09-19 08:28:26 -0700

    Add files via upload

--- Weights3GUI2/mainwindow.cpp
+++ Weights3GUI2/mainwindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->textEdit->setReadOnly(true);
 }
 
 MainWindow::~MainWindow()
@@ -49,32 +50,52 @@ QJsonObject get_json_object(const QByteArray bytes) {
     return obj;
 }
 
+QString extend(QString s1, QString s2) {
+    return s1 + s2 + "\n";
+}
 
-// "Open" pushbutton. Used to load cached JSON objects from Pycharm project folder.
+
+// creates a single string with line formatting (e.g. to be displayed on a QLabel widget)
+QString get_workout_text(const QVariantMap workout) {
+    QString result = "";
+    QVariantList exercises = workout["exercises"].toList();
+    for (auto iter1 = exercises.begin(); iter1 != exercises.end(); ++iter1) {
+        QVariantMap exercise = iter1->toMap();
+        QVariantList sets = exercise["sets"].toList();
+        result = extend(result, "TITLE: " + exercise["name"].toString());
+        result = extend(result, "Original: " + exercise["text"].toString());
+        for (auto iter2 = sets.begin(); iter2 != sets.end(); ++iter2) {
+            QVariantMap set_ = iter2->toMap();
+            QString weight = "Weight: " + set_["weight"].toString();
+            if (weight != "None") {
+                weight = weight + " lbs. ";
+            }
+            QString count = "Count: " + set_["count"].toString();
+            QString reps = "Reps : " + set_["reps"].toString();
+            result = extend(result, "\t " + weight + " | " + reps + " | " + count);
+          }
+    }
+    return result;
+}
+
+
+// The "Open" pushbutton is used to load cached JSON objects from Pycharm project folder.
 // NOTE: Code here was adapted from this helpful website by Erick Veil:
 // http://erickveil.github.io/2016/04/06/How-To-Manipulate-JSON-With-C++-and-Qt.html
-
 void MainWindow::on_pushButton_clicked()
 {
     QString cache_dir = "/Users/logan/PycharmProjects/Weights3/cache";
     QString filename = QFileDialog::getOpenFileName(this, "Open a file", cache_dir);
     const QByteArray json_bytes = get_json_bytes(filename);
     const QJsonObject json_object = get_json_object(json_bytes);
+    const QVariantMap workout = json_object.toVariantMap();
+    QString text = get_workout_text(workout);
+    ui->textEdit->append(text);
+}
 
-    // access data
-    QVariantMap workout = json_object.toVariantMap();
-    QVariantList exercises = workout["exercises"].toList();
-    for (auto iter1 = exercises.begin(); iter1 != exercises.end(); ++iter1) {
-        QVariantMap exercise = iter1->toMap();
-        QVariantList sets = exercise["sets"].toList();
-        qDebug() << "EXERCISE NAME: " << exercise["name"].toString();
-        qDebug() << "Original Line: " << exercise["text"].toString();
-        for (auto iter2 = sets.begin(); iter2 != sets.end(); ++iter2) {
-            QVariantMap set_ = iter2->toMap();
-            qDebug() << "weight is " << set_["weight"].toString();
-            qDebug() << "count is " << set_["count"].toString();
-            qDebug() << "reps is " << set_["reps"].toString();
-        }
-    }
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    delete ui;
 }
 
